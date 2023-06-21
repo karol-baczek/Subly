4564f07e7b1b5c5724200f9b7a48d2d8265fad28
karol-baczek <baczekkarol@gmail.com>
2023-06-21 21:09:57 +0200

    eh

--- Subly/Views/SubAddPage.xaml.cs
+++ Subly/Views/SubAddPage.xaml.cs
@@ -9,22 +9,34 @@ public partial class SubAddPage : ContentPage
     {
         InitializeComponent();
         BindingContext = newsub;
+        LocalNotificationCenter.Current.NotificationActionTapped += Current_NotificationActionTapped;
 
     }
 
+    private void Current_NotificationActionTapped(Plugin.LocalNotification.EventArgs.NotificationActionEventArgs e)
+    {
+        if (e.IsDismissed)
+        {
+
+        }
+        else if (e.IsTapped)
+        {
+
+        }
+    }
     private void AddClicked(object sender, EventArgs e)
     {   
             var request = new NotificationRequest
             {
                 NotificationId = 1337,
-                Title = "One of your subscriptions will renew soon",
+                Title = "One of your subscriptions will soon renew",
                 Subtitle = "Hello",
                 Description = "Hello",
                 BadgeNumber = 42,
                 Schedule = new NotificationRequestSchedule
                 {
                     NotifyTime = DateTime.Now.AddSeconds(6),
-                    NotifyRepeatInterval = TimeSpan.FromDays(1)
+                    NotifyRepeatInterval = TimeSpan.FromDays(30)
                 }
             };
         
